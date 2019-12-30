2695fc59b7b2fe11af0bdef8d56fde9c03c9d5b4
Nan Nguyen <hello@nann.ca>
2019-12-30 10:51:34 -0500

    Fixed bugs for logged out users

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,30 @@
 Rails.application.routes.draw do
-  
-  # SAME AS BELOW
-  # get 'users/:username' => 'users#show', as: 'user'
-  resources :users, only: :show, param: :username do
-    member do
-      post 'follow' => 'follows#create'
-      delete 'unfollow' => 'follows#destroy'
-    end
-  end
+	ActiveAdmin.routes(self)
+	devise_for :users
 
-  get 'feed' => 'feed#show'
+	as :user do
+		get 'signin' => 'devise/sessions#new'
+		delete 'signout' => 'devise/sessions#destroy'
+		get 'signup' => 'devise/registrations#new'
+	end
+	
+	# SAME AS BELOW
+	# get 'users/:username' => 'users#show', as: 'user'
+	resources :users, only: :show, param: :username do
+		member do
+			post 'follow' => 'follows#create'
+			delete 'unfollow' => 'follows#destroy'
+		end
+	end
 
-  resources :posts
-  resources :tweets
-  ActiveAdmin.routes(self)
-  devise_for :users
-  as :user do
-    get 'signin' => 'devise/sessions#new'
-  	delete 'signout' => 'devise/sessions#destroy'
-  	get 'signup' => 'devise/registrations#new'
-  end
-  root 'pages#home'
-  get 'about' => 'pages#about'
-  get 'contact' => 'pages#contact'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	get 'feed' => 'feed#show'
+
+	resources :posts
+	resources :tweets
+	
+
+	root 'pages#home'
+	get 'about' => 'pages#about'
+	get 'contact' => 'pages#contact'
+	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
