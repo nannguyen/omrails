8040dfac4ebf2a277631dde15a1b3ca6e1d2e5eb
Nan Nguyen <hello@nann.ca>
2020-01-04 17:28:29 -0500

    A bit of styling and character limit on Tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
   validates :user, presence: true
   has_attached_file :image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
